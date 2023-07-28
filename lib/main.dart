7ae540f4709a84207f86829720cdc1d3331016d4
KAMANDA ROGATI <kamandarogati@gmail.com>
2023-07-28 11:00:47 +0300

    budgetfile

---
+++ lib/main.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Financial App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 150,
+              height: 50,
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: ButtonWidget(
+                  label: 'Budget',
+                  onPressed: () {
+                    // Handle Budget button press here
+                  },
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 150,
+              height: 50,
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: ButtonWidget(
+                  label: 'Expenses',
+                  onPressed: () {
+                    // Handle Expenses button press here
+                  },
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 150,
+              height: 50,
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: ButtonWidget(
+                  label: 'Report',
+                  onPressed: () {
+                    // Handle Report button press here
+                  },
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 150,
+              height: 50,
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: ButtonWidget(
+                  label: 'Income',
+                  onPressed: () {
+                    // Handle Income button press here
+                  },
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 150,
+              height: 50,
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: ButtonWidget(
+                  label: 'History',
+                  onPressed: () {
+                    // Handle History button press here
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ButtonWidget extends StatelessWidget {
+  final String label;
+  final VoidCallback onPressed;
+
+  const ButtonWidget({super.key, 
+    required this.label,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: Text(label),
+    );
+  }
+}
---
+++ test/widget_test.dart
@@ -0,0 +1,36 @@
+//import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:saving_money/main.dart';
+
+void main() {
+  testWidgets('Test if buttons are displayed', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget( const MyApp());
+
+    // Verify if the buttons are displayed on the screen.
+    expect(find.text('Budget'), findsOneWidget);
+    expect(find.text('Expenses'), findsOneWidget);
+    expect(find.text('Report'), findsOneWidget);
+    expect(find.text('Income'), findsOneWidget);
+    expect(find.text('History'), findsOneWidget);
+  });
+
+ testWidgets('Test navigation to BudgetPage', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    // Tap the 'Budget' button and trigger navigation.
+    await tester.tap(find.text('Budget'));
+    await tester.pumpAndSettle();
+
+    // Verify if we're on the BudgetPage.
+    expect(find.text('This is the Budget Page'), findsOneWidget);
+  });
+
+  // You can add similar tests for the other buttons/pages as well.
+}
+//With these changes, when you run the app, tapping on any of the buttons ('Budget', 'Expenses', 'Report', 'Income', 'History') will navigate to the respective pages. Additionally, the widget tests will check if the buttons are displayed on the screen and verify that the navigation to the next pages is functioning correctly.
+
+
+
+
